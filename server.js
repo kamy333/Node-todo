1bff22d01d9a3152727ab089f34705b79de80ac9
Kamran Nafisspour <nafisspour@bluewin.ch>
2016-08-21 21:06:56 +0200

    Add POST for todos

--- server.js
+++ server.js
@@ -1,28 +1,18 @@
 var express=require('express');
+var bodyParser=require('body-parser');
+
 var app=express()  ;
 var PORT= process.env.PORT || 3000;
-var todos=[
-    {
-    id:1,
-    description:'Meet Adrian',
-    completed:false
-    },
-    {
-        id:2,
-        description:'Learn TypeScript',
-        completed:false
-    },
-    {
-        id:3,
-        description:'Learn Angular2 js',
-        completed:false
-    }
-    ];
+var todoNextId=1;
+var todos=[];
 
 app.get('/',function (req,res) {
-    res.send('ToDo API Root 2nd commit')
+    res.send( `ToDo API Root 2nd commit
+    <p><a href="/todos">ToDos</a></p>`);
+
 });
 
+app.use(bodyParser.json());
 
 app.get('/todos',function (req,res) {
     res.json(todos)
@@ -47,6 +37,22 @@ app.get('/todos/:id',function (req,res) {
 
 });
 
+app.post('/todos',function (req,res) {
+var body=req.body;
+    body.id=todoNextId++;
+    console.log('body.id '+body.id);
+
+    todos.push({
+        id:body.id,
+        description:body.description,
+        completed:body.completed
+    });
+
+    // console.log(`description `,body[0].description);
+    console.log('NextId '+ todoNextId, todos);
+
+    res.json(todos);
+});
 
 app.listen(PORT,function () {
     console.log(` Express running on port ${PORT}! `);

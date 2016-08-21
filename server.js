407cad517f57b881b11cef31771177c47c201bf4
Kamran Nafisspour <nafisspour@bluewin.ch>
2016-08-21 22:23:15 +0200

    Refactor with underscore and sanity check

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express=require('express');
 var bodyParser=require('body-parser');
+var _=require('underscore');
 
 var app=express()  ;
 var PORT= process.env.PORT || 3000;
@@ -20,13 +21,8 @@ app.get('/todos',function (req,res) {
 
 app.get('/todos/:id',function (req,res) {
     var todoId= parseInt(req.params.id);
-    var matchedTodo;
+    var matchedTodo=_.findWhere(todos, {id: todoId});
 
-    todos.forEach(function (todo) {
-        if(todo.id===todoId){
-            matchedTodo=todo;
-        }
-    });
 
     if(matchedTodo){
         res.json(matchedTodo);
@@ -38,22 +34,23 @@ app.get('/todos/:id',function (req,res) {
 });
 
 app.post('/todos',function (req,res) {
-var body=req.body;
+
+// var body=req.body;
+    var body=_.pick(req.body, 'description', 'completed');
+
+    if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length===0){
+        return res.status(400).send();
+    }
+
     body.id=todoNextId++;
+    body.description=body.description.trim();
     console.log('body.id '+body.id);
 
-    todos.push({
-        id:body.id,
-        description:body.description,
-        completed:body.completed
-    });
+    todos.push(body);
 
-    // console.log(`description `,body[0].description);
-    console.log('NextId '+ todoNextId, todos);
-
-    res.json(todos);
+    res.json(body);
 });
 
 app.listen(PORT,function () {
-    console.log(` Express running on port ${PORT}! `);
+    console.log(` Express running on port ${PORT}!`);
 });
